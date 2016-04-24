b149b97ddec6040e22b349fe02e2214c6def9a0d
Yusef Abouremeleh <yusef.abouremeleh@gmail.com>
2016-04-23 18:42:33 -0700

    On target activity score working. Chart now displays data

--- sgRNA Design Tool/crispr_app/views.py
+++ sgRNA Design Tool/crispr_app/views.py
@@ -6,7 +6,7 @@ from .forms import NumberForm
 from Bio.Seq import Seq
 from Bio import Entrez, SeqIO
 import sys
-sys.path.insert(0, '/Users/Yusef/Documents/Kitematic/crisprapp/hello_django_docker/Project/crispr_app/Rule_Set_2_scoring_v1/analysis')
+sys.path.insert(0, '/Users/Yusef/Documents/Kitematic/crisprapp/hello_django_docker/sgRNA Design Tool/crispr_app/Rule_Set_2_scoring_v1/analysis')
 from rs2_score_calculator import calculateScore
 import cPickle as pickle
 import dill
@@ -32,38 +32,70 @@ def index(request):
 	return HttpResponse(template.render(context,request))
 
 def form(request):
+	#Load Template
 	template = loader.get_template('crispr_app/form.html')
+
+	#Initialize context variables
 	sequence = ''
-	scores = ''
-	indices = ''
+	sgRNAInfo = []
+
 	if request.method == 'POST':
-		#Get sequence
-		# gi = request.POST.get('locus')
-		# start = request.POST.get('start')
-		# stop = request.POST.get('end')
+		#Refresh page if fields empty
+		# if not gi and not start and not stop:
+		# 	return HttpResponse(template.render({}, request))
+
+		#Get user input 
+		gi = request.POST.get('locus')
+		start = request.POST.get('start')
+		stop = request.POST.get('end')
+
+		#Fetch sequence data
 		Entrez.email = 'yabourem@ucsd.edu'
 		handle = Entrez.efetch(db='nucleotide',id='EU490707.1',rettype='gb',retmode='text', seq_start=1, seq_stop=200)
+		#handle = Entrez.efetch(db='nucleotide',id=gi,rettype='gb',retmode='text', seq_start=start, seq_stop=stop)
 		record = SeqIO.read(handle, 'gb')
 		sequence = record.seq
 		handle.close()
-		print type(sequence)
 		sequence = str(sequence)
-		#Get scores of sequence
+
+		#Calculate scores of sequence, and collect other sgRNA information
 		model_file = open('crispr_app/V3_model_nopos.pickle', 'rb')
 		model = pickle.load(model_file)
 		scores = []
 		indices = []
+		complements = []
+		PAMs = []
+		#5-->3
 		for i in range(len(sequence) - 30):
 			toScore = sequence[i:i+30]
 			if len(toScore) == 30 and toScore[25:27] == 'GG':
-				print i
+				complements.append(toScore[4:24])
+				PAMs.append(toScore[24:27])
 				scores.append(calculateScore(toScore, model)) 
 				indices.append(i)
-		
+		#3-->5 (Reverse complement)
+		mySeq = Seq(sequence)
+		reverseComp = str(mySeq.reverse_complement())
+		for i in range(len(reverseComp) - 30):
+			toScore = reverseComp[i:i+30]
+			if len(toScore) == 30 and toScore[25:27] == 'GG':
+				complements.append(toScore[4:24])
+				PAMs.append(toScore[24:27])
+				scores.append(calculateScore(toScore, model)) 
+				indices.append(i)
+				print i
+				print reverseComp
+
+		#Put all sgRNA info into one array
+		#Note to self: careful with indices, as the user input starts at 1
+		for i in range(len(scores)):
+			temp = [scores[i], indices[i], complements[i], PAMs[i]]
+			sgRNAInfo.append(temp)
+
 	context = {
 	'seq' : sequence,
-	'scores' : scores,
-	'indices' : indices
+	'sgRNAInfo' : sgRNAInfo
+
 	}
 	return HttpResponse(template.render(context, request))
 
@@ -71,6 +103,5 @@ def form(request):
 def result(request):
 	template = loader.get_template('crispr_app/result.html')
 	context = {
-	'hi' : 1
 	}
 	return HttpResponse(template.render(context, request))
